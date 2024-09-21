131478fa746a4ce69e4bc3626c17fb6d3a9f3102
Reynaldo Vinsen <100607241+Sushrimp@users.noreply.github.com>
2024-09-21 08:37:01 +0700

    Time: 4 ms (88.45%), Space: 12.3 MB (93.86%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
-    static bool comparator(int a, int b){
-        return to_string(a) < to_string(b);
+    int fetchNext(int num, int n){
+        if(num * 10 <= n) return num*10;
+        
+        if(num >= n) num /= 10;
+
+        num++;
+        while(num%10 == 0){
+            num /= 10;
+        }
+
+        return num;
     }
 
     vector<int> lexicalOrder(int n) {
         vector<int> res;
 
-        for (int i = 1; i <= n; ++i) {
-            res.push_back(i);
-        }
+        int curr = 1;
 
-        sort(res.begin(), res.end(), comparator);
+        for(int i = 0 ; i < n ; i++){
+            res.push_back(curr);
+            curr = fetchNext(curr, n);
+        }
 
         return res;
     }
